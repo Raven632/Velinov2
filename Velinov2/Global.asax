b7504e86cccae4c77d7804c6ba2e9c84c755e6e1
Raven632 <48404748+Raven632@users.noreply.github.com>
2019-03-24 12:09:41 +0300

    1.0

---
+++ Velinov2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Velinov2.MvcApplication" Language="C#" %>
---
+++ Velinov2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Velinov2.Startup))]
+namespace Velinov2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
